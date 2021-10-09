b5baf119afa1f1e30d06804daa598621ebb90680
Radha solanki <rps3308@gmail.com>
2021-10-09 14:45:18 +0100

    This is my first commit

---
+++ HomePage.java
@@ -0,0 +1,15 @@
+package com.demo.nopcommerce;
+
+public class HomePage {
+    // Main method to execute the programme
+    public static void main(String[] args) {
+
+        // print  statement to print some text.
+
+        System.out.println("My First Java Program");
+
+    }
+
+
+}
+
---
+++ LoginPage.java
@@ -0,0 +1,17 @@
+package com.demo.nopcommerce;
+
+
+public class LoginPage {
+
+
+
+
+
+
+
+    }
+
+
+
+
+
